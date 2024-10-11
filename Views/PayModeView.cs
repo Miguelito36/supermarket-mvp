033ae0bcefbb8a61f56d1671ea534eb743f4c57a
Jose Miguel Astaiza <jomiasga@gmail.com>
2024-10-10 23:02:13 -0500

    Pay Mode Presenter

--- Views/PayModeView.cs
+++ Views/PayModeView.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,96 @@ using System.Windows.Forms;
 
 namespace Supermarket_mvp.Views
 {
-    public partial class PayModeView : Form
+    public partial class PayModeView : Form, IPayModeView
     {
+        private bool isEdit;
+        private bool isSuccessful;
+        private string message;
         public PayModeView()
         {
             InitializeComponent();
+            AssociateAndRaiseViewEvents();
+
+            tabControl1.TabPages.Remove(tabPagePayModeDetail);
         }
+
+        public string PayModeId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string PayModeName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string PayModeObservation { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string SearchValue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string IsEdit { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string IsSuccessful { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string Message { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+
+
+        public event EventHandler SearchEvent;
+        public event EventHandler AddNewEvent;
+        public event EventHandler EditEvent;
+        public event EventHandler DeleteEvent;
+        public event EventHandler SaveEvent;
+        public event EventHandler CancelEvent;
+        public event EventHandler Searchevent;
+
+        public string PayModeId
+        {
+            get { return TxtPayModeId.Text; }
+            set { TxtPayModeId.Text = value; }
+        }
+        public string PayModeName
+        {
+            get { return TxtPayModeName.Text; }
+            set { TxtPayModeName.Text = value; }
+
+        }
+        public string PayModeObservation
+        {
+            get { return TxtPayModeObservation.Text; }
+            set { TxtPayModeObservation.Text = value; }
+        }
+        public string SearchValue
+        {
+            get { return Txtsearch.Text; }
+            set { Txtsearch.Text = value; }
+        }
+        public bool IsEdit
+        {
+            get { return isEdit; }
+            set { IsEdit = value; }
+        }
+        public bool IsSuccessful
+        {
+            get { return isSuccessful; }
+            set { IsSuccessful = value; }
+        }
+        public string Message
+        {
+            get { return Message; }
+            set { message = value; }
+        }
+        public void SetPayModeListBildingSource(BindingSource payModeList)
+        {
+            DgPayMode.DataSource = payModeList;
+        }
+        private void AssociateAndRaiseViewEvents()
+        {
+            BtnSearch.Click += delegate { SearchEvent?.Invoke(this, EventArgs.Empty); };
+            Txtsearch.KeyDown += (s, e) =>
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    SearchEvent?.Invoke(this, EventArgs.Empty);
+                }
+            };
+        }
+
+        public string SearchValues { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsSuccessfull { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
+
